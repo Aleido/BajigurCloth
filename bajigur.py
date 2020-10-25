dabd28509d0e6a4fcb410abf0d2e9ec879130ea8
aleido <ridho1030@gmail.com>
2020-10-25 22:14:50 +0700

    add inventory and order API access

--- bajigur.py
+++ bajigur.py
@@ -47,6 +47,34 @@ class Product(db.Model):
         self.ukuran = ukuran
         self.harga = harga
 
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    kodeSKU = db.Column(db.String(50), unique=True)
+    tanggalmasuk = db.Column(db.DateTime)
+    tanggalupdate = db.Column(db.DateTime)
+    stock = db.Column(db.Integer)
+    lokasi = db.Column(db.String(30))
+
+    def __init__(self, kodeSKU, tanggalmasuk, tanggalupdate, stock, lokasi):
+        self.kodeSKU = kodeSKU 
+        self.tanggalmasuk = tanggalmasuk 
+        self.tanggalupdate = tanggalupdate 
+        self.stock = stock 
+        self.lokasi = lokasi
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer)
+    tanggalorder = db.Column(db.DateTime)
+    amount = db.Column(db.Integer)
+    status = db.Column(db.String(20))
+
+    def __init__(self, userID, tanggalorder, amount, status):
+        self.userID = userID 
+        self.tanggalorder = tanggalorder 
+        self.amount = amount 
+        self.status = status 
+
 
 db.create_all()
 
@@ -59,7 +87,7 @@ class UserSchema(ma.Schema):
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
-# Inputan Data User
+# Inputan Data User / Customer
 @app.route('/user', methods=['Post'])
 def create_user():
   nama = request.json['nama']
@@ -107,12 +135,11 @@ def delete_user(id):
   db.session.commit()
   return user_schema.jsonify(user)
 
-#========================= product
+#========================= PRODUCT
 class ProductSchema(ma.Schema):
     class Meta:
         fields = ('id', 'kodeSKU',  'namabarang', 'bahan', 'warna', 'ukuran', 'harga' )
 
-# Generating object of UserSchema for single object and for multiple object
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
@@ -173,8 +200,138 @@ def delete_product(id):
   db.session.commit()
   return product_schema.jsonify(product)
 
+#========================= INVENTORY
+class InventorySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'kodeSKU', 'tanggalmasuk', 'tanggalupdate', 'stock', 'lokasi' )
 
-# Simple Checking API
+inventory_schema = InventorySchema()
+inventories_schema = InventorySchema(many=True)
+
+# Inputan Inventory product
+@app.route('/inventory', methods=['Post'])
+def create_inventory():
+  kodeSKU = request.json['kodeSKU'] 
+  tanggalmasuk = request.json['tanggalmasuk']
+  tanggalupdate = request.json['tanggalupdate']
+  stock = request.json['stock']
+  lokasi = request.json['lokasi'] 
+
+  new_inventory= Inventory(kodeSKU, tanggalmasuk, tanggalupdate, stock, lokasi )
+
+  db.session.add(new_inventory)
+  db.session.commit()
+
+  return inventory_schema.jsonify(new_inventory)
+
+@app.route('/inventory', methods=['GET'])
+def get_inventories():
+  all_inventories = Inventory.query.all()
+  result = inventories_schema.dump(all_inventories)
+  return jsonify(result)
+
+@app.route('/inventory/<id>', methods=['GET'])
+def get_inventory(id):
+  inventory = Inventory.query.get(id)
+  return inventory_schema.jsonify(inventory)
+
+@app.route('/inventory/<id>', methods=['PUT'])
+def update_inventory(id):
+  inventory = Inventory.query.get(id)
+  
+  kodeSKU = request.json['kodeSKU'] 
+  tanggalmasuk = request.json['tanggalmasuk']
+  tanggalupdate = request.json['tanggalupdate']
+  stock = request.json['stock']
+  lokasi = request.json['lokasi'] 
+
+
+  inventory.kodeSKU = kodeSKU
+  inventory.tanggalmasuk = tanggalmasuk
+  inventory.tanggalupdate = tanggalupdate
+  inventory.stock = stock
+  inventory.lokasi = lokasi 
+
+  db.session.commit()
+
+  return inventory_schema.jsonify(inventory)
+
+@app.route('/inventory/<id>', methods=['DELETE'])
+def delete_inventory(id):
+  inventory = Inventory.query.get(id)
+  db.session.delete(inventory)
+  db.session.commit()
+  return inventory_schema.jsonify(inventory)
+
+#========================= ORDER
+class OrderSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'userID', 'tanggalOrder', 'amount', 'status' )
+
+order_schema = OrderSchema()
+orders_schema = OrderSchema(many=True)
+
+# Inputan Order 
+@app.route('/order', methods=['Post'])
+def create_order():
+  userID = request.json['userID'] 
+  tanggalorder = request.json['tanggalorder']
+  amount = request.json['amount']
+  status = request.json['status'] 
+
+  new_order= Order(userID, tanggalorder, amount, status )
+
+  db.session.add(new_order)
+  db.session.commit()
+
+  return order_schema.jsonify(new_order)
+
+@app.route('/order', methods=['GET'])
+def get_orders():
+  all_order = Order.query.all()
+  result = orders_schema.dump(all_order)
+  return jsonify(result)
+
+@app.route('/order/<id>', methods=['GET'])
+def get_order(id):
+  order = Order.query.get(id)
+  return order_schema.jsonify(order)
+
+@app.route('/order/<id>', methods=['PUT'])
+def update_order(id):
+  order = Order.query.get(id)
+  
+  userID = request.json['userID'] 
+  tanggalorder = request.json['tanggalorder']
+  amount = request.json['amount']
+  status = request.json['status'] 
+
+  order.userID = userID 
+  order.tanggalorder = tanggalorder 
+  order.amount = amount 
+  order.status = status
+
+  db.session.commit()
+
+  return inventory_schema.jsonify(order)
+
+@app.route('/order/<id>', methods=['DELETE'])
+def delete_order(id):
+  order = Order.query.get(id)
+  db.session.delete(order)
+  db.session.commit()
+  return order_schema.jsonify(order)
+
+# Cek API TOKO
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'ini adalah backend system toko Bajigur Cloth'})
+
+# It will run flask Application
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# Cek API TOKO
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'message': 'ini adalah backend system toko Bajigur Cloth'})
